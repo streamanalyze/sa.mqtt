d2cb058e16dc16c9d2a1fc0b8aa05bdee178e97d
Johan Risch <johan.risch@gmail.com>
2026-04-10 11:06:27 +0200

    fix: correct payload length calculation in mqtt_publishfn

--- src/sa_mqtt.c
+++ src/sa_mqtt.c
@@ -256,7 +256,7 @@ ohandle mqtt_publishfn(bindtype env, ohandle instance, ohandle topic,
   } else {
     sa_json_stringify(&str, value);
     pubmsg.payload = strdup(getstring(str));
-    pubmsg.payloadlen = strlen(str);
+    pubmsg.payloadlen = strlen(pubmsg.payload);
   }
 
   pubmsg.qos = context->qos;
